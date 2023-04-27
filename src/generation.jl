7d19b9217ce1821e95b5d1091c713b76b5e768ea
ericbdt <eric@LAPTOP-6DVD499O>
2023-04-27 15:49:05 +0200

    generation added

--- src/generation.jl
+++ src/generation.jl
@@ -1,18 +1,92 @@
 # This file contains methods to generate a data set of instances (i.e., sudoku grids)
-include("io.jl")
+include("io_singles.jl")
 
-"""
-Generate an n*n grid with a given density
 
-Argument
-- n: size of the grid
-- density: percentage in [0, 1] of initial values in the grid
-"""
-function generateInstance(n::Int64, density::Float64)
+function generateInstance(n::Int64)
 
-    # TODO
-    println("In file generation.jl, in method generateInstance(), TODO: generate an instance")
-    
+    G=Matrix{Int64}(ones(n,n))
+
+    nb_black = rand(0:(div(n+1,2)))
+    top_black=[]
+    for i in 1:n
+
+        #determination des cases noires
+        black = []
+        sides = []
+        for j in 1:nb_black
+            k = rand(1:n)
+            while k in top_black || k in sides || k in black
+                println("1")
+                k = rand(1:n)
+            end
+            println("sorti")
+            G[i,k] = 0 
+            push!(black,k)
+            if k-1 >=1
+                push!(sides,k-1)
+            end
+            if k+1<=n
+                push!(sides,k+1)
+            end
+        end
+        
+        #valeurs des cases blanches
+        for j in 1:n 
+            if G[i,j] == 1
+                k = rand(1:n)
+                while k in G[i,1:(j-1)] || k in G[1:(i-1),j]
+                    k = rand(1:n)    
+                    println("2")
+                end
+                G[i,j]=k
+            end
+        end
+
+        nb_black = rand(0:(div(n+1,2) - nb_black))
+        top_black = black
+
+        
+    end
+
+    #ajout des valeurs des cases noires
+
+    for i in 1:n 
+        for j in 1:n 
+            if G[i,j] == 0
+                choices = [G[i,:];G[:,j]]
+                k = rand(choices)
+                while k == 0
+                    k = rand(choices)
+                    println("3")
+                end
+                G[i,j] = k
+            end
+        end
+    end
+
+
+    for i in 1:n 
+        println(G[i,:])
+    end
+
+
+    fichier = open("data/singles_taille_"*string(n)*".txt","w")
+    line=""
+    for k in 1:n
+        line=""
+        for l in 1:n-1
+            line = line*string(G[k,l])
+            line = line*","
+        end
+        line = line*string(G[k,n])
+        line = line*"\n"
+        write(fichier, line)
+    end
+    close(fichier)
+
+
+
+
 end 
 
 """
